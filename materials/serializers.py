d0856b9f7d5b48425b62ea3785ca810ef1313cc5
Sergey Korolev <bongrichard@rambler.ru>
2024-07-02 16:48:57 +0300

    добавил вывод подписки в CourseDitailSerializer

--- materials/serializers.py
+++ materials/serializers.py
@@ -34,13 +34,20 @@ class CourseSerializer(ModelSerializer):
 
 class CourseDitailSerializer(serializers.ModelSerializer):
     number_lessons = serializers.SerializerMethodField(read_only=True)
+    subscription = serializers.SerializerMethodField(read_only=True)
 
     def get_number_lessons(self, course):
         return Lesson.objects.filter(course=course).count()
 
+    def get_subscription(self, odj):
+        user = User.objects.get(is_active=True)
+        if odj.subscription.filter(owner=user).count() < 1:
+            return "нет подписки"
+        return "есть подписка"
+
     class Meta:
         model = Course
-        fields = ['id', 'name', 'description', 'number_lessons']
+        fields = ['id', 'name', 'description', 'number_lessons', 'subscription']
 
 class SubscriptionSerializer(serializers.ModelSerializer):
     class Meta:
